64f3c43930f2cfb0da48e2f1b388f7a44492debe
Omar Elbaz <you@example.com>
2025-12-31 12:49:19 +0200

    made some changes in index.js

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ app.use(morgan("combined"));
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: 150,
   message: {
     status: "Too Many Requests",
     error: "Too many login attempts, please try again later.",
@@ -59,8 +59,8 @@ const authLimiter = rateLimit({
   legacyHeaders: false,
 });
 
-app.use("/api/users/login");
-app.use("/api/users/register");
+app.use("/api/users/login", authLimiter);
+app.use("/api/users/register", authLimiter);
 
 app.use("/uploads", express.static(uploadsDir));
 app.use("/api/users", usersRouter);
